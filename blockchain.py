fa00bd232edc27d3e021e800741b6b6cb71a459a
KrzysztofMiler <krzy.miler@gmail.com>
2019-04-08 13:09:30 +0200

    inut

--- blockchain.py
+++ blockchain.py
@@ -1,15 +1,18 @@
-blockchain = [1]
+blockchain = []
 
 
 def get_last_blockchain_val():
     return blockchain[-1]
 
 
-def add_value(transaction_amount):
-    blockchain.append([get_last_blockchain_val(), transaction_amount])
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_amount])
 
 
 add_value(5)
 
-print(blockchain)
 
+tx_amount = float(input("Podaj wartość transakcji "))
+add_value(tx_amount)
+add_value(last_transaction=get_last_blockchain_val(), transaction_amount=tx_amount)
+print(blockchain)
