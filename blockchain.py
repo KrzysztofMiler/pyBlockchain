3b19589110e56aa6dba540d22bf62452cbfbde47
KrzysztofMiler <krzy.miler@gmail.com>
2019-04-08 19:31:07 +0200

    Dodanie tworzenia bloków i ich walidacji

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,14 @@
 # Stat blockchain'a
-blockchain = []
+genesis_block = {'previous_hash': '',
+                 'index': 0,
+                 'transactions': []}
+blockchain = [genesis_block]
+open_transactions = []
+owner = 'me'
+
+
+def hash_block(block):
+    return '-'.join([str(block[key]) for key in block])
 
 
 def get_last_blockchain_val():
@@ -10,23 +19,33 @@ def get_last_blockchain_val():
         return blockchain[-1]
 
 
-def add_transaction(transaction_amount, last_transaction=get_last_blockchain_val()):
+def add_transaction(recipient, sender=owner, amount=1.0):
     """Dodaj nową wartość na koniec blokchaina
 
     Arg:
-        :transaction_amount: wartość dodawanej transakcji
+        :sender: wysyłajacy transakcje
 
-        :last_transaction: wartość ostatniej transakcji (domyślnie wywołuje get_last_blockchain_val() która zwraaca ostanią wartoś,
-        jeśli jest to pierwsze wywołanie i blokchain jest pusty to wstawi domyslną wartość [1])
+        :recipient: odbierający transakcje
+        :amount: wartość transakcji (domyślnie 1.0)
     """
-    if last_transaction == None:
-        last_transaction = [1]
-    blockchain.append([last_transaction, transaction_amount])
+    transaction = {'sender': sender, 'recipient': recipient, 'amount': amount}
+    open_transactions.append(transaction)
+
+
+def mine_block():
+    """Tworzy natępny blok w blockchain"""
+    last_block = blockchain[-1]
+    hashed_block = hash_block(last_block)  # tutaj optymalizacja
+
+    block = {'previous_hash': hashed_block,
+             'index': len(blockchain),
+             'transactions': open_transactions}
+    blockchain.append(block)
 
 
 def get_transaction_val():
-    """Przyjmuje liczbę od użytkownika ,zamienia na float i zwraca"""
-    return float(input("Podaj wartość transakcji "))
+    """Przyjmuje adres odbierającego i liczbę od użytkownika ,zwraca str adresata i float wartości"""
+    return (input('Podaj odbierającego transkcje '), float(input("Podaj wartość transakcji ")))
 
 
 def get_user_choice():
@@ -38,35 +57,42 @@ def print_blockchain_elements():
     """Wypisuje w pętli wszystkie bloki w blockchainie"""
     for block in blockchain:
         print(block)
+    else:
+        print('-'*30)
+
 
 def verify_chain():
-    block_index = 0
-    is_valid = True
-    for block in blockchain:
-        if block_index == 0:
-            block_index += 1
+    """Weryfikuje blockchain za pomocą sprawdzenia czy poprzednie fragmenty są takie same, zwraca bool True jeśli jest porawny i False jeśli jest zmanipulowany"""
+    for (index, block) in enumerate(blockchain):
+        if index == 0:  # zobaczyć czy się nie da inaczej by nie marnować cykli
             continue
-        elif block[0] == blockchain[block_index - 1]:
-            is_valid = True
-        else:
-            is_valid = False
-            break
-        block_index += 1
-    return is_valid
+        if block['previous_hash'] != hash_block(blockchain[index-1]):
+            return False
+    return True
 
-while True:
+
+wait_for_input = True
+
+
+while wait_for_input:
     print("Wybierz akcje ")
-    print('1 Nowa wartość transakcji \n2 Wyświetl bloki blockchaina \nh Aby zmodyfikować \nq Aby wyjść')
+    print('1 Nowa wartość transakcji \n2 Mine nowy blok \n3 Wyświetl bloki blockchaina \nh Aby zmodyfikować \nq Aby wyjść')
     user_choice = get_user_choice()
     if user_choice == '1':
-        add_transaction(get_transaction_val(), get_last_blockchain_val())
+        recepient, amount = get_transaction_val()
+        add_transaction(recepient, amount=amount)
+        # print(open_transactions)
     elif user_choice == '2':
+        mine_block()
+    elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == 'q':
-        break
+        wait_for_input = False
     elif user_choice == 'h':
         if len(blockchain) >= 1:
-            blockchain[0]=[2]
+            blockchain[0] = {'previous_hash': '',
+                             'index': 0,
+                             'transactions': [{'sender': 'XD', 'recipient': 'dsad', 'amount': 1e6}]}
     else:
         print('Niepoprawna cyfra ')
 
@@ -75,3 +101,5 @@ while True:
         break
 
     print("Zarejestrowano wybór")
+else:
+    print("Użytkownik wyszedł")
